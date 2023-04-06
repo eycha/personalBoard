058413c4d9e88ff6a9151385adcda10cec8d6d08
eycha <silvlexonst@gmail.com>
2023-04-06 12:06:47 +0900

    feat: posting the DB

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.study.board.controller;
 
 import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,12 +10,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
+    @Autowired
+    private BoardService boardService;
     @GetMapping("/board/write")//localhost:8090/board/write
     public String boardWriteForm() {
         return "boardWrite";
     }
     @PostMapping("board/writepro")
     public String boardWritePro(Board board) {
+        boardService.write(board);
         System.out.println(board.getTitle());
         return "";
     }
